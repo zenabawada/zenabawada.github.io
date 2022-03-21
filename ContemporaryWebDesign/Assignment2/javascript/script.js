b19ce0a87f81abbfb86254cbfd4e126ebbb2d71e
zenabawada <zenawada@outlook.com>
2022-03-20 23:35:14 -0400

    Assignment2

---
+++ ContemporaryWebDesign/Assignment2/javascript/script.js
@@ -0,0 +1,9 @@
+let icon = document.getElementById('navIcon');
+icon.onclick = function() {
+    let topNav = document.getElementById('topNav');
+    if (topNav.className === 'topnav') {
+        topNav.className += ' responsive';
+    } else {
+        topNav.className = 'topnav';
+    }
+}
