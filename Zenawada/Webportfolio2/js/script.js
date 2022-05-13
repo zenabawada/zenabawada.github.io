319cc4f6cca1c0c983cbf0a7a2c35b32e3e8e57b
zenabawada <zenawada@outlook.com>
2022-05-12 23:42:57 -0400

    Update

--- Zenawada/Webportfolio2/js/script.js
+++
@@ -1,9 +0,0 @@
-let nav = document.getElementById("siteNav");
-
-nav.onclick = function() {
-    if (nav.className === "nav-list") {
-        nav.className += " responsive";
-    } else {
-        nav.className = "nav-list";
-    }
-}
