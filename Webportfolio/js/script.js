e254e1d6ef5769694d69f0c1d060dc1bef271567
zenabawada <zenawada@outlook.com>
2022-05-27 23:42:50 -0400

    Update project

--- Webportfolio/js/script.js
+++ Webportfolio/js/script.js
@@ -1,9 +1,24 @@
-let nav = document.getElementById("siteNav");
+// let nav = document.getElementById("siteNav");
 
-nav.onclick = function() {
-    if (nav.className === "nav-list") {
-        nav.className += " responsive";
-    } else {
-        nav.className = "nav-list";
-    }
+// nav.onclick = function() {
+//     if (nav.className === "nav-list") {
+//         nav.className += " responsive";
+//     } else {
+//         nav.className = "nav-list";
+//     }
+// }
+
+let mobileNavIcon = document.getElementById("mobileNavIcon");
+let closeBtn = document.getElementById("closeBtn");
+let navContainer = document.getElementById("navContainer");
+let mainWrap = document.getElementById("mainWrap");
+const mediaQueryMobile = window.matchMedia("(max-width: 680px)");
+const mediaQueryWeb = window.matchMedia("(min-width: 680px");
+
+mobileNavIcon.onclick = function() {
+    navContainer.className += " responsive";
+}
+
+closeBtn.onclick = function() {
+    navContainer.className = "nav-container";
 }
